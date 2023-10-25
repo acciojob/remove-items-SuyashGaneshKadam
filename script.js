031d076f00bb6d5d8afc72077a261be787d7cd47
Suyash Kadam <122480150+SuyashGaneshKadam@users.noreply.github.com>
2023-10-25 18:25:28 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 //your JS code here. If required.
+let select = document.getElementById("colorSelect");
+let button = document.querySelector("input");
+button.addEventListener("click", remove);
+function remove(){
+	let value = select.value;
+	let children = select.children;
+	for(let i in children){
+		if(children[i].innerText == value){
+			children[i].remove();
+			break;
+		}
+	}
+}
